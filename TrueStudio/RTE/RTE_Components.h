51dbff0e8498949eccbb136be2b3db822071ab52
Dung Do Dang <dung@neo.no>
2018-07-02 10:37:14 +0200

    Add project configuration for Atollic TrueStudio/
    Atollic TrueStudio is a free of charge GCC based IDE for STM32 ARM Cortex M microcontrollers.
    It is a very popular and good IDE.
    
    Go to www.atollic.com for more information.

---
+++ TrueStudio/RTE/RTE_Components.h
@@ -0,0 +1,27 @@
+/*
+ * Auto generated Run-Time-Environment Component Configuration File
+ *      *** Do not modify ! ***
+ *
+ * Project: TCD1304-CCD-STM32F7-Disco
+ * RTE configuration: TCD1304-CCD-STM32F7-Disco.rteconfig
+*/
+#ifndef RTE_COMPONENTS_H
+#define RTE_COMPONENTS_H
+
+/*
+ * Define the Device Header File:
+*/
+#define CMSIS_device_header "stm32f7xx.h"
+
+#define RTE_DEVICE_FRAMEWORK_CLASSIC
+#define RTE_DEVICE_HAL_ADC
+#define RTE_DEVICE_HAL_COMMON
+#define RTE_DEVICE_HAL_CORTEX
+#define RTE_DEVICE_HAL_DMA
+#define RTE_DEVICE_HAL_GPIO
+#define RTE_DEVICE_HAL_PWR
+#define RTE_DEVICE_HAL_RCC
+#define RTE_DEVICE_HAL_TIM
+#define RTE_DEVICE_HAL_UART
+
+#endif /* RTE_COMPONENTS_H */
